b111b4b4026bcfe4bb4caaa83cc16ec18ab60569
Dylan Stephano-Shachter <dylan@theone.ninja>
2026-03-14 17:07:09 +0200

    delete migration

--- migrations/002_subscriber_confirm_default.sql
+++
@@ -1,3 +0,0 @@
--- Change default for new subscribers to unconfirmed (requires email confirmation).
--- Existing confirmed subscribers are left as-is.
-ALTER TABLE subscribers ALTER COLUMN confirmed SET DEFAULT false;
